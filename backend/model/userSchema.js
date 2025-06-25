e5f0a79df02d1c4bff4e39d27cd86810baebe3c7
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-06-25 23:04:32 +0530

    Added user and book schemas

---
+++ backend/model/userSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('moongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports= User;
