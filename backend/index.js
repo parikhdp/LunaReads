02c9a56c9342e3eea38a55e7e372caca86b93246
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-06-26 17:49:22 +0530

    Typos and errors in routes corrected. nodemon connected to database

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bookRoute = require("./routes/bookRoutes");
+const authRoute = require("./routes/authRoutes");
+const bodyParser = require("body-parser"); //to identify json data
+
+require('./db');
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.json());
+
+
+// Configure CORS
+const corsOptions = {
+    origin: '*', // Allow all origins (for development). For production will have to specify domains
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
+  app.use(cors(corsOptions));
+  
+app.use("/api/auth", authRoute);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- backend/model/userSchema.js
+++ backend/model/userSchema.js
@@ -1,4 +1,4 @@
-const mongoose = require('moongoose');
+const mongoose = require('mongoose'); 
 
 const userSchema = new mongoose.Schema({
     name: {
