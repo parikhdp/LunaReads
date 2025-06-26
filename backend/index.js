0138a4b530f6e534d71b702fac5260e82494b204
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-06-26 18:31:09 +0530

    Added bookRoutes

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,16 @@ app.use(express.json());
 
 // Configure CORS
 const corsOptions = {
-    origin: '*', // Allow all origins (for development). For production will have to specify domains
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  };
-  app.use(cors(corsOptions));
-  
+  origin: '*', // Allow all origins (for development). For production will have to specify domains
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
+
+app.use("/api/books", bookRoute);
 app.use("/api/auth", authRoute);
+
+app.use('/uploads', express.static('uploads'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
